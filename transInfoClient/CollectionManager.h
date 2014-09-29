e19654d2dfcd2ec5c7fe6d1de8f84766516a4554
Omar Soto <omar.soto2@upr.edu>
2014-09-29 14:26:19 -0400

    More work on transInfoClient.

---
+++ transInfoClient/CollectionManager.h
@@ -0,0 +1,13 @@
+//
+//  CollectionManager.h
+//  transinfo
+//
+//  Created by Omar Soto Fortuño on 9/25/14.
+//  Copyright (c) 2014 University of Puerto Rico Mayagüez Campus. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface CollectionManager : NSObject
+
+@end
---
+++ transInfoClient/CollectionManager.m
@@ -0,0 +1,13 @@
+//
+//  CollectionManager.m
+//  transinfo
+//
+//  Created by Omar Soto Fortuño on 9/25/14.
+//  Copyright (c) 2014 University of Puerto Rico Mayagüez Campus. All rights reserved.
+//
+
+#import "CollectionManager.h"
+
+@implementation CollectionManager
+
+@end
