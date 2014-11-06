72f1a468daa05c20d72fc69cf0e271fd49b29138
Omar Soto <omar.soto2@upr.edu>
2014-11-06 16:26:43 -0400

    More work on Data Entry tab and services in the Server.

---
+++ transInfoClient/NewEntryViewController.h
@@ -0,0 +1,13 @@
+//
+//  NewEntryViewController.h
+//  transinfo
+//
+//  Created by Omar Soto Fortuño on 11/6/14.
+//  Copyright (c) 2014 University of Puerto Rico Mayagüez Campus. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface NewEntryViewController : UIView
+
+@end
---
+++ transInfoClient/NewEntryViewController.m
@@ -0,0 +1,13 @@
+//
+//  NewEntryViewController.m
+//  transinfo
+//
+//  Created by Omar Soto Fortuño on 11/6/14.
+//  Copyright (c) 2014 University of Puerto Rico Mayagüez Campus. All rights reserved.
+//
+
+#import "NewEntryViewController.h"
+
+@implementation NewEntryViewController
+
+@end
