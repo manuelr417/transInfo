7030a0795841c5dcf39cffa7bd0cbbc52b29569a
Omar Soto <omar.soto2@upr.edu>
2015-03-02 10:07:34 -0400

    Fixes on Tab 3, Tab 4. Changed the Vehicle Extended Information to Push from Popover. Initial work on Tab 5.

---
+++ transInfoClient/PersonExtendedViewController.h
@@ -0,0 +1,13 @@
+//
+//  PersonExtendedViewController.h
+//  transinfo
+//
+//  Created by Omar Soto Fortuño on 3/2/15.
+//  Copyright (c) 2015 University of Puerto Rico Mayagüez Campus. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface PersonExtendedViewController : UIViewController
+
+@end
